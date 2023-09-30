936d6fbc654379062f5c472ed5f777a210dbaedf
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-09-30 16:55:47 +1000

    Add RGB randomisation

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function resetGrid() {
     return false;
   }
 
-  modal.close();
+  modal.close();s
   generateGrid(sideCount, sideCount);
   return true;
 }
@@ -28,12 +28,46 @@ function generateGrid(rows = 16, cols = 16) {
       
       cell.addEventListener('mouseenter', (e) => {
         const cell = e.target;
-        cell.style.backgroundColor = 'white';
+        cell.style.backgroundColor = generateHsl(cell.style.backgroundColor);
       });
       
       row.appendChild(cell);
     }
   }
+}
+
+function generateHsl(oldColor) {
+  console.log('test:' + oldColor);
+  if (oldColor.slice(0, 3) !== 'rgb') {
+    return `hsl(${Math.floor(Math.random() * 360)},` +
+               `${Math.floor(Math.random() * 101)}%,` +
+               `50%)`;
+  } else {
+    const hsl = rgbToHsl(...oldColor.replace(/[^\d,]/g, '').split(','));
+    console.log(Math.round(hsl[2] * 100 - 5));
+    return `hsl(${Math.floor(Math.random() * 360)},` +
+               `${Math.floor(Math.random() * 100)}%,` +
+               `${Math.round(hsl[2] * 100 - 5)}%)`;
+  }
+}
+
+function rgbToHsl(r, g, b) {
+  (r /= 255), (g /= 255), (b /= 255);
+  const vmax = Math.max(r, g, b), vmin = Math.min(r, g, b);
+  let h, s, l = (vmax + vmin) / 2;
+
+  if (vmax === vmin) {
+    return [0, 0, l]; // achromatic
+  }
+
+  const d = vmax - vmin;
+  s = l > 0.5 ? d / (2 - vmax - vmin) : d / (vmax + vmin);
+  if (vmax === r) h = (g - b) / d + (g < b ? 6 : 0);
+  if (vmax === g) h = (b - r) / d + 2;
+  if (vmax === b) h = (r - g) / d + 4;
+  h /= 6;
+
+  return [h, s, l];
 }
 
 function init() {
