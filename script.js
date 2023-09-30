eea24a57b99910c52597fa108db41625546ab18f
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-09-30 13:54:47 +1000

    Add core functionality

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-const rows = 16;
-const cols = 16;
-const container = document.querySelector("#grid");
+function resetGrid() {
+  const modal = document.querySelector('#reset-modal');
+  const rows = parseInt(modal.querySelector('#modal-row-count').value);
+  const cols = parseInt(modal.querySelector('#modal-column-count').value);
+  
+  modal.close();
+  
+  if (!Number.isFinite(rows) || !Number.isFinite(cols) ||
+      rows < 1 || cols < 1) {
+    return;
+  }
 
-for (let i = 0; i < rows; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  container.appendChild(row);
+  generateGrid(rows, cols);
+}
 
-  for (let j = 0; j < cols; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
+function generateGrid(rows = 16, cols = 16) {
+  const container = document.querySelector("#grid");
 
-    row.appendChild(cell);
+  container.replaceChildren();
+  
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+    
+    for (let j = 0; j < cols; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      
+      cell.addEventListener('mouseenter', (e) => {
+        const cell = e.target;
+        cell.style.backgroundColor = 'black';
+      });
+      
+      row.appendChild(cell);
+    }
   }
 }
+
+function init() {
+  const resetButton = document.querySelector('#reset-button');
+  const modal = document.querySelector('#reset-modal');
+  const submitButton = document.querySelector('#modal-submit');
+  const cancelButton = document.querySelector('#modal-cancel');
+
+  // Add modal button events
+  resetButton.addEventListener('click', () => modal.showModal());
+  submitButton.addEventListener('click', resetGrid);
+  cancelButton.addEventListener('click', () => modal.close());
+  
+  generateGrid();
+}
+
+init();
